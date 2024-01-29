1f72a927b2c0cb57f2ed74523d379f89ee9e3836
Álvaro García Barragán <79256834+Alvaro8gb@users.noreply.github.com>
2024-01-29 14:39:50 +0100

    improve readability

--- GPT/gptFormat.py
+++ GPT/gptFormat.py
@@ -61,39 +61,3 @@ def decoder(text_formated:str):
 
 def normalizer(text:str)->list[str]:
     return text.strip().split()
-
-def decoder_old(input_text:str, llm_text:str):
-    y_pred = []
-
-    conten_text = normalizer(input_text)
-    candidate_sentence_list = normalizer(llm_text)
-
-    print(candidate_sentence_list)
-
-    flag = False
-    start_ = 0
-
-    for word_idx, word in enumerate(candidate_sentence_list):
-        #print(word, y_pred)
-        if len(word) > 2 and word[0] == '@' and word[1] == '@':
-            flag = True
-            for end_ in range(word_idx, len(candidate_sentence_list)):
-                end_word = candidate_sentence_list[end_]
-                if len(end_word) > 2 and end_word[-1] == '#' and end_word[-2] == '#':
-                    entity_ = " ".join(candidate_sentence_list[word_idx:end_ + 1])[2:-2]
-                    len_ = end_ - word_idx + 1
-                    while start_ < len(conten_text):
-                        if start_ + len_ - 1 < len(conten_text) and " ".join(
-                                conten_text[start_:start_ + len_]) == entity_:
-                            y_pred.append((" ".join(conten_text[start_:start_ + len_]), start_, start_ + len_ - 1))
-                            break
-                        start_ += 1
-                    break
-            if len(word) > 2 and word[-1] == '#' and word[-2] == '#':
-                flag = False
-                continue
-
-            if not flag:
-                start_ += 1
-        
-    return y_pred 
